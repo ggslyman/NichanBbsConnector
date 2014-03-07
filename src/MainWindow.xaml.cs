e21426a1105baab23fac6bd91332787e7c7da864
ggslyman <ggslyman@gmail.com>
2014-03-07 13:13:41 +0900

    レイアウトをGridからDockPanelへ変更

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using NichanUrlParserUi;
 using NichanUrlParser;
 using System.Collections;
 using System.Windows.Forms;
+using System.Collections.ObjectModel;
 
 namespace NichanUrlParserUi
 {
@@ -26,6 +27,38 @@ namespace NichanUrlParserUi
     {
         static public MainWindow Instance;
         private static NichanUrlParser.NichanUrlParser nps;
+        private ObservableCollection<bbs> listBbs;
+
+        private async Task getUrl(string url)
+        {
+            nps.setUrl(url);
+            nps.setUrls();
+            await nps.setNamesAndSubjectsAsync();
+            addMsg(nps.ListSubjects.Count.ToString());
+            setLabel();
+        }
+
+        private void setLabel()
+        {
+            this.Title = nps.ThreadName + " - " + nps.BbsName;
+            //labelBase.Content = nps.BaseUrl;
+            //labelThread.Content = nps.ThreadUrl;
+            //labelDat.Content = nps.DatUrl;
+            //labelBbsName.Content = ;
+            //labelThreadName.Content = nps.ThreadName;
+        }
+
+        private void addMsg(string msg)
+        {
+            textMsg.Text = msg + "\n" + textMsg.Text;
+
+        }
+        private void webLoadControlEnabled(bool enabled){
+            dataGridSubjects.IsEnabled = enabled;
+            listBoxUrl.IsEnabled = enabled;
+            buttonSetUrl.IsEnabled = enabled;
+        }
+
         public MainWindow()
         {
             try
@@ -42,83 +75,72 @@ namespace NichanUrlParserUi
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             dataGridSubjects.ItemsSource = nps.ListSubjects;
-            listViewThreadView.ItemsSource = nps.TreadLineSubject;
+            listViewThreadView.ItemsSource = nps.ListTreadLines;
             listBoxUrl.SelectedIndex = 0;
         }
 
         private async void listBoxUrl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            dataGridSubjects.IsEnabled = false;
-            listBoxUrl.IsEnabled = false;
-            buttonSetUrl.IsEnabled = false;
+            webLoadControlEnabled(false);
             if (listBoxUrl.SelectedIndex >= 0)
             {
                 await getUrl(listBoxUrl.SelectedItem.ToString());
                 await nps.getThreadLines();
             }
-            buttonSetUrl.IsEnabled = true;
-            listBoxUrl.IsEnabled = true;
-            dataGridSubjects.IsEnabled = true;
+            webLoadControlEnabled(true);
         }
 
         private void formMain_Loaded(object sender, RoutedEventArgs e)
         {
             //listBoxUrl.SelectedIndex = 0;
         }
-        // スレ一覧の項目ダブルクリック
-        private async void dataGridSubjects_MouseDoubleClik(object sender, MouseButtonEventArgs e)
-        {
-            dataGridSubjects.IsEnabled = false;
-            listBoxUrl.IsEnabled = false;
-            buttonSetUrl.IsEnabled = false;
-            NichanUrlParser.Subject subject = (NichanUrlParser.Subject)dataGridSubjects.SelectedItem;
-    
-            await getUrl(subject.Url);
-            await nps.getThreadLines();
-            buttonSetUrl.IsEnabled = true;
-            listBoxUrl.IsEnabled = true;
-            dataGridSubjects.IsEnabled = true;
-        }
-
 
         private async void buttonSetUrl_Click(object sender, RoutedEventArgs e)
         {
-            dataGridSubjects.IsEnabled = false;
-            listBoxUrl.IsEnabled = false;
-            buttonSetUrl.IsEnabled = false;
+            webLoadControlEnabled(false);
             listBoxUrl.Items.Add(textBoxUrl.Text);
             
             await getUrl(textBoxUrl.Text);
             await nps.getThreadLines();
 
-            buttonSetUrl.IsEnabled = true;
-            listBoxUrl.IsEnabled = true;
-            dataGridSubjects.IsEnabled = true;
+            webLoadControlEnabled(true);
         }
 
-        private async Task getUrl(string url)
+        private async void dataGridSubjects_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            nps.setUrl(url);
-            nps.setUrls();
-            await nps.setNamesAndSubjectsAsync();
-            setLabel();
+            if (dataGridSubjects.Items.Count > 0)
+            {
+                webLoadControlEnabled(false);
+                NichanUrlParser.Subject subject = (NichanUrlParser.Subject)dataGridSubjects.SelectedItem;
+
+                await getUrl(subject.Url);
+                await nps.getThreadLines();
+
+                webLoadControlEnabled(true);
+            }
         }
 
-        private void setLabel()
+    }
+    class bbs
+    {
+        private string title;
+        private string url;
+        private int order;
+        public int Order
         {
-            labelBase.Content = nps.BaseUrl;
-            labelThread.Content = nps.ThreadUrl;
-            labelDat.Content = nps.DatUrl;
-            labelBbsName.Content = nps.BbsName;
-            labelThreadName.Content = nps.ThreadName;
+            get { return order; }
+            set { order = value; }
         }
-        
-        private void addMsg(string msg)
+        public string Title
         {
-            textMsg.Text = msg + "\n" + textMsg.Text;
-
+            get { return title; }
+            set { title = value; }
         }
-
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
     }
 
 }
